ef1a52167f70cdd0e463cd3057b838dbc7f13c4c
yanomamo <yanomamo@gmail.com>
2015-06-26 00:51:24 -0400

    routes fixed

--- public/core.js
+++ public/core.js
@@ -3,21 +3,51 @@ var scotchTodo = angular.module('scotchTodo', [])
     '$compileProvider', '$routeProvider',
     function( $compileProvider, $routeProvider )
     {   
+      var getUserInfo = function ($http) {
+        return $http.get('/api/users/sessionUser')
+          .success(function(data) {
+            console.log(data);
+            return data;
+          })
+          .error(function(data) {
+            console.log('Error: ' + data);
+            return null;
+          });
+      };
+
       $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|data):/);
-      $routeProvider.when('/library', {
+      $routeProvider.when('/login', {
+        templateUrl: 'templates/login',
+        controller: 'loginController',
+        resolve: {
+          userInfo: ['$http', getUserInfo]
+        }
+      }).when('/library', {
         templateUrl: 'templates/library',
-        controller: 'mainController'
+        controller: 'libraryController',
+        resolve: {
+          userInfo: ['$http', getUserInfo]
+        }
       }).when('/search', {
         templateUrl: 'templates/search',
-        controller: 'mainController'
+        controller: 'searchController',
+        resolve: {
+          userInfo: ['$http', getUserInfo]
+        }
       }).when('/upload', {
         templateUrl: 'templates/upload',
-        controller: 'mainController'
+        controller: 'uploadController',
+        resolve: {
+          userInfo: ['$http', getUserInfo]
+        }
       }).when('/settings', {
         templateUrl: 'templates/settings',
-        controller: 'mainController'
+        controller: 'settingsController',
+        resolve: {
+          userInfo: ['$http', getUserInfo]
+        }
       }).otherwise({
-        redirectTo: '/'
+        redirectTo: '/search'
       });
     }
 ]);
@@ -45,95 +75,31 @@ scotchTodo.directive('ngSearch', function() {
 });
 
 // Main controller!
-function mainController($scope, $http) {
-  $scope.formData = {};
-  $scope.searchOption = 0;
-  $scope.user = {};
 
-  // check if the session has a user
-  $http.get('/api/users/sessionUser')
-    .success(function(data) {
-      $scope.user = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
+scotchTodo.controller('mainController',
+  function mainController($scope, $http, $location) {
+    $scope.formData = {};
+    $scope.searchOption = 0;
+    $scope.user = {};
 
+    $scope.goTo = function (path) {
+      $location.path(path);
+    }
 
-  // when landing on the page, get all todos and show them
-  /*
-  $http.get('/api/presets')
-    .success(function(data) {
-      $scope.presets = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-  */
+    $scope.logout = function () {
+      $http.post('/api/users/sessionDelete')
+        .success(function() {
+          $scope.user = {};
+          console.log('logged out!');
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    }
 
-  $scope.createUser = function (username, password) {
-    // get user to see if they exist
-    $http.post('/api/users', {
-        username: username,
-        password: password
-      }).success(function(data) {
-        if (data.username) {
-          $scope.user = data;
-        } else {
-          console.log(data.error);
-        }
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-  }
-
-  $scope.login = function (username, password) {
-    $http.post('/api/users/login', {
-      username: username,
-      password: password
-    })
-      .success(function(data) {
-        if (data.username) {
-          $scope.user = data;
-        } else {
-          console.log(data.error);
-        }
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-  }
-
-  $scope.logout = function () {
-    $http.post('/api/users/sessionDelete')
-      .success(function() {
-        $scope.user = {};
-        console.log('logged out!');
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-  }
-
-  // delete a todo after checking it
-  $scope.deleteTodo = function(id) {
-    $http.delete('/api/presets/' + id)
-      .success(function(data) {
-        $scope.presets = data;
-        console.log(data);
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-  };
-
-  //UNTESTED
-  $scope.updateResults = function(userName, presetName, tags, fileType) {
-    if (tags && fileType && false) {
-      $http.get('/api/presets/' + tags[0] + '/' + tags[1] + '/' + tags[2])
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+      $http.delete('/api/presets/' + id)
         .success(function(data) {
           $scope.presets = data;
           console.log(data);
@@ -141,57 +107,130 @@ function mainController($scope, $http) {
         .error(function(data) {
           console.log('Error: ' + data);
         });
-    }
+    };
 
-    console.log(userName);
-    console.log(presetName);
-    console.log(tags);
-    console.log(fileType);
-
-    console.log('searching!!');
-  }
-
-  var handleFileSelect = function(evt) {
-    var files = evt.target.files;
-    var paused = true;
-
-    if (files) {
-
-      // for each file uplaoded, encode
-      var i = 0;
-      var loop = setInterval(function(){
-        var filename = files[i].name;
-        var n = filename.replace(/\.[^/.]+$/, "");
-        var e = filename.substr(filename.lastIndexOf('.'));
-
-        var reader = new FileReader();
-        reader.onload = function(readerEvt) {
-          var binaryString = readerEvt.target.result;
-
-          // send file to server
-          $http.post('/api/presets', {
-            name: n,
-            extension: e,
-            file: btoa(binaryString)
+    //UNTESTED
+    $scope.updateResults = function(userName, presetName, tags, fileType) {
+      if (tags && fileType && false) {
+        $http.get('/api/presets/' + tags[0] + '/' + tags[1] + '/' + tags[2])
+          .success(function(data) {
+            $scope.presets = data;
+            console.log(data);
           })
-            .success(function(data) {
-              $scope.presets = data;
-              console.log(data);
-            })
-            .error(function(data) {
-              console.log('Error: ' + data);
-            });
-        };
-        reader.readAsBinaryString(files[i]);
+          .error(function(data) {
+            console.log('Error: ' + data);
+          });
+      }
 
-        i++;
-        if(i === files.length) {
-            clearInterval(loop);
-        }
-      }, 100);
+      console.log(userName);
+      console.log(presetName);
+      console.log(tags);
+      console.log(fileType);
+
+      console.log('searching!!');
     }
-  };
 
-  //identifies upload button
-  //document.getElementById('file-upload').addEventListener('change', handleFileSelect, false);
-}
+    var handleFileSelect = function(evt) {
+      var files = evt.target.files;
+      var paused = true;
+
+      if (files) {
+
+        // for each file uplaoded, encode
+        var i = 0;
+        var loop = setInterval(function(){
+          var filename = files[i].name;
+          var n = filename.replace(/\.[^/.]+$/, "");
+          var e = filename.substr(filename.lastIndexOf('.'));
+
+          var reader = new FileReader();
+          reader.onload = function(readerEvt) {
+            var binaryString = readerEvt.target.result;
+
+            // send file to server
+            $http.post('/api/presets', {
+              name: n,
+              extension: e,
+              file: btoa(binaryString)
+            })
+              .success(function(data) {
+                $scope.presets = data;
+                console.log(data);
+              })
+              .error(function(data) {
+                console.log('Error: ' + data);
+              });
+          };
+          reader.readAsBinaryString(files[i]);
+
+          i++;
+          if(i === files.length) {
+              clearInterval(loop);
+          }
+        }, 100);
+      }
+    };
+
+    //identifies upload button
+    //document.getElementById('file-upload').addEventListener('change', handleFileSelect, false);
+  }
+);
+
+scotchTodo.controller('searchController',
+  function ($scope, $http, userInfo) {
+    $scope.$parent.user = userInfo.data;
+  });
+
+scotchTodo.controller('loginController',
+  function ($scope, $http, userInfo) {
+    $scope.user = userInfo.data;
+    $scope.$parent.user = $scope.user;
+
+    $scope.login = function (username, password) {
+      $http.post('/api/users/login', {
+        username: username,
+        password: password
+      })
+        .success(function(data) {
+          if (data.username) {
+            $scope.user = data;
+          } else {
+            console.log(data.error);
+          }
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+    $scope.createUser = function (username, password) {
+      // get user to see if they exist
+      $http.post('/api/users', {
+          username: username,
+          password: password
+        }).success(function(data) {
+          if (data.username) {
+            $scope.user = data;
+          } else {
+            console.log(data.error);
+          }
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+  });
+
+scotchTodo.controller('libraryController', 
+  function ($scope, $http, userInfo) {
+    $scope.$parent.user = userInfo.data;
+  });
+
+scotchTodo.controller('uploadController', function ($scope, $http, userInfo) {
+    $scope.$parent.user = userInfo.data;
+  });
+
+scotchTodo.controller('settingsController', function ($scope, $http, userInfo) {
+    $scope.$parent.user = userInfo.data;
+  });
+
